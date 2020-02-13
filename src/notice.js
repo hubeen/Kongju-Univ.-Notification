58ea4d13356ebea66c486697192f4087780b3218
Seonggwon Yoon <keyakoto@gmail.com>
2020-02-13 22:38:04 +0900

    update: notice.js
    Change var to let and code convention

--- src/notice.js
+++ src/notice.js
@@ -1,36 +1,37 @@
-const request = require("request");
-const cheerio = require("cheerio");
-const iconv = require("iconv-lite");
+const request = require('request');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
 const log = console.log;
-const notic_student = "http://www.kongju.ac.kr/lounge/board.jsp?board=student_news&page="
-function GetData(i){
-    request({
-         method:'GET',
-         uri: notic_student + i,
-         headers: { "User-Agent": "Mozilla/5.0" },
-         encoding: null
-     }, function (err, res, html){
-        var data = [];
-        const strContents = Buffer(html);
-        const body= iconv.decode(strContents, 'EUC-KR').toString();
-        const $ = cheerio.load(body);
-        var table = $("table.content_main_table02");
-        var tr_list = $(table).children( "tbody" ).children("tr.table_tr");
+const notic_student = 'http://www.kongju.ac.kr/lounge/board.jsp?board=student_news&page='
 
-        for( var row = 0; row < tr_list.length; row++ ) {
-            var cells = tr_list.eq( row ).children( );
-            var cols = [ ];
-            for( var column = 0; column < cells.length -1; column++ ) {
-                if(column != 2){
-                    var hero = cells.eq( column ).text().replace(/(\\n)|(\s){2,}|(\\t)/g, "");
-                    cols.push( hero );
-                }
-            }
-            data.push( cols );
-         }
-         log(data);
-     });
+function GetData(i) {
+  request({
+    method:'GET',
+    uri: notic_student + i,
+    headers: { 'User-Agent': 'Mozilla/5.0' },
+    encoding: null
+  }, function (err, res, html) {
+    let data = [];
+    const strContents = Buffer(html);
+    const body= iconv.decode(strContents, 'EUC-KR').toString();
+    const $ = cheerio.load(body);
+    let table = $('table.content_main_table02');
+    let tr_list = $(table).children('tbody').children('tr.table_tr');
+    for (let row = 0; row < tr_list.length; row++) {
+      let cells = tr_list.eq(row).children();
+      let cols = [];
+      for (let column = 0; column < cells.length - 1; column++) {
+        if (column != 2) {
+            let hero = cells.eq(column).text().replace(/(\\n)|(\s){2,}|(\\t)/g, '');
+            cols.push(hero);
+        }
+      }
+      data.push(cols);
+    }
+    log(data);
+  });
 }
-for(var i=0; i<3; i++){
-    GetData(i);
+
+for (let i = 0; i < 3; i++) {
+  GetData(i);
 }
